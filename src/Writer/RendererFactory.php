cf8ab0cb9a60260aeda7818c6a5892a98b9af4f8
Jérémy DECOOL <contact@jdecool.fr>
2017-05-28 16:16:24 +0200

    Allow to add custom renderer

--- src/Writer/RendererFactory.php
+++ src/Writer/RendererFactory.php
@@ -8,6 +8,16 @@ class RendererFactory
 {
     const VERSION_1 = '1.0';
 
+    private $renderers;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->renderers = [];
+    }
+
     /**
      * Create specific renderer
      *
@@ -16,11 +26,29 @@ class RendererFactory
      */
     public function createRenderer($version = self::VERSION_1)
     {
+        if (isset($this->renderers[$version])) {
+            return $this->renderers[$version];
+        }
+
         switch ($version) {
             case self::VERSION_1:
                 return new Version1\Renderer();
         }
 
         throw new InvalidArgumentException(sprintf('Unknow version "%s".', $version));
+    }
+
+    /**
+     * Register a custom renderer
+     *
+     * @param string $key
+     * @param RendererInterface $renderer
+     * @return RendererFactory
+     */
+    public function registerRenderer($key, RendererInterface $renderer)
+    {
+        $this->renderers[$key] = $renderer;
+
+        return $this;
     }
 }
